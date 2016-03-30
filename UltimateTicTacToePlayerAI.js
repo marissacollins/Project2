28a557a9f1e6cfaa2abd17e537014a739cec5aad
bcharlesUMD <bcharles@umassd.edu>
2016-03-30 11:30:19 -0400

    Added description of search and evaluation function to the AI code

---
+++ UltimateTicTacToePlayerAI.js
@@ -0,0 +1,10 @@
+/*	AI 1: Easy mode using depth-bound, min-max search with evaluation function
+	
+	Evaluation function:
+	4 Criteria to be used when evaluating a move
+	
+	-Gain one point for each opponent character in a row from potential space
+	-Gain one point for placing your character on the board
+	-Gain one point for each of your characters in a row from potential space
+	-Lose one point for each of your characters in a row with an opponent's character and the potential space
+ */
