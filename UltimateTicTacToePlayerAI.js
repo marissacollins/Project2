cb14c653523f60ae962631502c882c01dd4defbd
bcharlesUMD <bcharles@umassd.edu>
2016-03-30 14:48:37 -0400

    Continuing work on the evaluation switch statement. Currently working on 0.1

--- UltimateTicTacToePlayerAI.js
+++ UltimateTicTacToePlayerAI.js
@@ -23,6 +23,7 @@
 					[0,0,0]];
 	 
 	 var checkVal;
+	 var spaceScore = 0;
 	 
 	//Check the value of possible moves in game x,y
 	for(var i = 0; i < 3; i++){ //Check rows (x)
@@ -34,17 +35,88 @@
 			else{ //Otherwise, calculate the value
 				switch(checkVal){
 					case "0.0": //Calculate top-left value
+						spaceScore = 1; //Gain 1 point for putting a character in the space
 						for (var x = 0; x < 3; x++){
 							for (var y = 0; y < 3; y++){
-								
+								if((x == 0 && y == 0) || (x == 1 && y == 2) || (x == 2 && y == 1)){ //Skip: top-left, center-right, bottom-middle
+									//Skip
+								}
+								else{
+									if(selected[outerX, outerY, x, y] == 1){ //If the space is taken by an opponent, gain 1 point for blocking
+										spaceScore++;
+									}
+									if(selected[outerX, outerY, x, y] == 2){ //If the space is taken by your character, gain 1 point for being in a row
+										spaceScore++;
+									}
+								}	
 							}
 						}
+						if ((selected[outerX, outerY, 0, 1] == 2 && selected[outerX, outerY, 0, 2] == 1) || (selected[outerX, outerY, 0, 1] == 1 && selected[outerX, outerY, 0, 2] == 2)){ //Lose 1 point if the top row is blocked
+							spaceScore--;
+						}
+						if ((selected[outerX, outerY, 1, 1] == 2 && selected[outerX, outerY, 2, 2] == 1) || (selected[outerX, outerY, 1, 1] == 1 && selected[outerX, outerY, 2, 2] == 2)){ //Lose 1 point if the diaganol row is blocked
+							spaceScore--;
+						}
+						if ((selected[outerX, outerY, 1, 0] == 2 && selected[outerX, outerY, 2, 0] == 1) || (selected[outerX, outerY, 1, 0] == 1 && selected[outerX, outerY, 2, 0] == 2)){ //Lose 1 point if the left column is blocked
+							spaceScore--;
+						}
+						valTable[i,j] = spaceScore;
 						break;
 					case "0.1": //Calculate top-middle value
-						
+						spaceScore = 1; //Gain 1 point for putting a character in the space
+						for (var x = 0; x < 3; x++){
+							for (var y = 0; y < 3; y++){ //Skip current space
+								if(x == 0 && y == 1){ //Skip: top-middle
+									//Skip
+								}
+								else{
+									if(selected[outerX, outerY, x, y] == 1){ //If the space is taken by an opponent, gain 1 point for blocking
+										spaceScore++;
+									}
+									if(selected[outerX, outerY, x, y] == 2){ //If the space is taken by your character, gain 1 point for being in a row
+										spaceScore++;
+									}
+								}	
+							}
+						}
+						if ((selected[outerX, outerY, 2, 1] == 2 && selected[outerX, outerY, 2, 2] == 1) || (selected[outerX, outerY, 2, 1] == 1 && selected[outerX, outerY, 2, 2] == 2)){ //Lose 1 point if the bottom row is blocked
+							spaceScore--;
+						} 
+						if ((selected[outerX, outerY, 1, 1] == 2 && selected[outerX, outerY, 0, 2] == 1) || (selected[outerX, outerY, 1, 1] == 1 && selected[outerX, outerY, 0, 2] == 2)){ //Lose 1 point if the diaganol row is blocked
+							spaceScore--;
+						}
+						if ((selected[outerX, outerY, 0, 0] == 2 && selected[outerX, outerY, 1, 0] == 1) || (selected[outerX, outerY, 0, 0] == 1 && selected[outerX, outerY, 1, 0] == 2)){ //Lose 1 point if the left column is blocked
+							spaceScore--;
+						}
+						valTable[i,j] = spaceScore;
 						break;
 					case "0.2": //Calculate top-right value
-						
+						spaceScore = 1; //Gain 1 point for putting a character in the space
+						for (var x = 0; x < 3; x++){
+							for (var y = 0; y < 3; y++){
+								if((x == 0 && y == 2) || (x == 1 && y == 0) || (x == 2 && y == 1)){ //Skip: top-right, center-left, bottom-middle
+									//Skip
+								}
+								else{
+									if(selected[outerX, outerY, x, y] == 1){ //If the space is taken by an opponent, gain 1 point for blocking
+										spaceScore++;
+									}
+									if(selected[outerX, outerY, x, y] == 2){ //If the space is taken by your character, gain 1 point for being in a row
+										spaceScore++;
+									}
+								}	
+							}
+						}
+						if ((selected[outerX, outerY, 0, 0] == 2 && selected[outerX, outerY, 0, 1] == 1) || (selected[outerX, outerY, 0, 0] == 1 && selected[outerX, outerY, 0, 1] == 2)){ //Lose 1 point if the top row is blocked
+							spaceScore--;
+						}
+						if ((selected[outerX, outerY, 1, 1] == 2 && selected[outerX, outerY, 2, 0] == 1) || (selected[outerX, outerY, 1, 1] == 1 && selected[outerX, outerY, 2, 0] == 2)){ //Lose 1 point if the diaganol row is blocked
+							spaceScore--;
+						}
+						if ((selected[outerX, outerY, 1, 2] == 2 && selected[outerX, outerY, 2, 2] == 1) || (selected[outerX, outerY, 1, 2] == 1 && selected[outerX, outerY, 2, 2] == 2)){ //Lose 1 point if the right column is blocked
+							spaceScore--;
+						}
+						valTable[i,j] = spaceScore;
 						break;
 					case "1.0": //Calculate center-left value
 						
@@ -56,13 +128,63 @@
 						
 						break;
 					case "2.0": //Calculate bottom-left value
-						
+						spaceScore = 1; //Gain 1 point for putting a character in the space
+						for (var x = 0; x < 3; x++){
+							for (var y = 0; y < 3; y++){
+								if((x == 0 && y == 1) || (x == 1 && y == 2) || (x == 0 && y == 2)){ //Skip: top-middle, center-right, bottom-left
+									//Skip
+								}
+								else{
+									if(selected[outerX, outerY, x, y] == 1){ //If the space is taken by an opponent, gain 1 point for blocking
+										spaceScore++;
+									}
+									if(selected[outerX, outerY, x, y] == 2){ //If the space is taken by your character, gain 1 point for being in a row
+										spaceScore++;
+									}
+								}	
+							}
+						}
+						if ((selected[outerX, outerY, 2, 1] == 2 && selected[outerX, outerY, 2, 2] == 1) || (selected[outerX, outerY, 2, 1] == 1 && selected[outerX, outerY, 2, 2] == 2)){ //Lose 1 point if the bottom row is blocked
+							spaceScore--;
+						} 
+						if ((selected[outerX, outerY, 1, 1] == 2 && selected[outerX, outerY, 0, 2] == 1) || (selected[outerX, outerY, 1, 1] == 1 && selected[outerX, outerY, 0, 2] == 2)){ //Lose 1 point if the diaganol row is blocked
+							spaceScore--;
+						}
+						if ((selected[outerX, outerY, 0, 0] == 2 && selected[outerX, outerY, 1, 0] == 1) || (selected[outerX, outerY, 0, 0] == 1 && selected[outerX, outerY, 1, 0] == 2)){ //Lose 1 point if the left column is blocked
+							spaceScore--;
+						}
+						valTable[i,j] = spaceScore;
 						break;
 					case "2.1": //Calculate bottom-middle value
 						
 						break;
 					case "2.2": //Calculate bottom-right value
-						
+						spaceScore = 1; //Gain 1 point for putting a character in the space
+						for (var x = 0; x < 3; x++){
+							for (var y = 0; y < 3; y++){
+								if((x == 0 && y == 1) || (x == 1 && y == 0) || (x == 2 && y == 2)){ //Skip: top-middle, center-left, bottom-right
+									//Skip
+								}
+								else{
+									if(selected[outerX, outerY, x, y] == 1){ //If the space is taken by an opponent, gain 1 point for blocking
+										spaceScore++;
+									}
+									if(selected[outerX, outerY, x, y] == 2){ //If the space is taken by your character, gain 1 point for being in a row
+										spaceScore++;
+									}
+								}	
+							}
+						}
+						if ((selected[outerX, outerY, 2, 0] == 2 && selected[outerX, outerY, 2, 1] == 1) || (selected[outerX, outerY, 2, 0] == 1 && selected[outerX, outerY, 2, 1] == 2)){ //Lose 1 point if the bottom row is blocked
+							spaceScore--;
+						} 
+						if ((selected[outerX, outerY, 1, 1] == 2 && selected[outerX, outerY, 0, 0] == 1) || (selected[outerX, outerY, 1, 1] == 1 && selected[outerX, outerY, 0, 0] == 2)){ //Lose 1 point if the diaganol row is blocked
+							spaceScore--;
+						}
+						if ((selected[outerX, outerY, 0, 2] == 2 && selected[outerX, outerY, 1, 2] == 1) || (selected[outerX, outerY, 0, 2] == 1 && selected[outerX, outerY, 1, 2] == 2)){ //Lose 1 point if the right column is blocked
+							spaceScore--;
+						}
+						valTable[i,j] = spaceScore;
 						break;
 				}
 			}
